9f48d11c6b41230eeea17a98912ff97bab1cef28
Vladimir <dippeepthegood@gmail.com>
2026-02-16 20:40:33 +0300

    inter

--- main.cpp
+++ main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    short a;
+    cout << "napishite dlinnu massiva: ";
+    cin>> a ;
+    //cout << "dlinna: " << a;
+    int arr[a];
+    char c;
+    cout << "kak hotite zapolnit? Sami(S)/Auto(A): ";
+    cin >> c;
+    switch (c){
+    case 'S': for(int i = 0; i<a; i++){
+            cout << "vvedite chlen [" << i + 1 << "]:";
+            cin >> arr[i];
+        } break;
+    case 'A': for(int i =0; i<a; i++){
+            arr[i] = -100 + rand() % 100;
+        }break;
+    }
+    for(int i = 0; i<a; i++){
+        cout<< "chlen[" << i+1 << "]:"<<arr[i]<<endl;
+    }
+
     return 0;
 }
