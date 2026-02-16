9ff21152d6c9c5f65aaa0dfe3c1d8f7551566e6b
Vladimir <dippeepthegood@gmail.com>
2026-02-16 20:59:57 +0300

    finalV

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main()
     short a;
     cout << "napishite dlinnu massiva: ";
     cin>> a ;
-    //cout << "dlinna: " << a;
     int arr[a];
     char c;
     cout << "kak hotite zapolnit? Sami(S)/Auto(A): ";
@@ -21,8 +20,24 @@ int main()
             arr[i] = -100 + rand() % 100;
         }break;
     }
+    cout << "CHECK:"<< endl;
     for(int i = 0; i<a; i++){
-        cout<< "chlen[" << i+1 << "]:"<<arr[i]<<endl;
+        cout<< "chlen[" << i+1 << "]: "<<arr[i]<<endl;
+    }
+    for(int i = 1; i<a-1;i++){
+        int Imin = 0;
+        for(int j = i+1; j<a;j++){
+            if(arr[j]< arr[Imin]){
+                Imin = j;
+            }
+        }
+        if(Imin != i){
+            swap(arr[i], arr[Imin]);
+        }
+    }
+    cout << endl << "OTSORTIROV. by Choice:"<< endl;
+    for(int i = 0; i<a; i++){
+        cout<< "chlen[" << i+1 << "]: "<<arr[i]<<endl;
     }
 
     return 0;
