efd062ff2412298921f1d7c7c81a4e28c9c81331
Vladimir <dippeepthegood@gmail.com>
2026-03-11 14:35:17 +0300

    3D

--- main.cpp
+++ main.cpp
@@ -3,6 +3,30 @@
 
 using namespace std;
 
+int getMaxBit(const int* array, int N){
+    int count = 0;
+    int MAXcount = 0;
+    int MAXindex = 0;
+    for(int i= 0; i< N;i++){
+        int x = array[i];
+        if(x > 0){
+            while(x>0){
+                if (x & 1) count++;
+                x >>= 1;
+            }
+            if(count > MAXcount){
+                MAXcount = count;
+                MAXindex = i;
+                count = 0;
+            }
+            else count = 0;
+        }
+        else continue;
+    }
+    if(MAXcount != 0) return MAXindex;
+    else return -1;
+}
+
 int main()
 {
     unsigned int w = 0xFFAABBCC;
@@ -12,10 +36,16 @@ int main()
     cout<<"------------"<<endl;
 
     unsigned int Pnumber = 0x80000801;
-    int n = 1;
-    int left = 0;
     cycleShift(&Pnumber, 3, 0);
-    cout << hex << Pnumber << endl;
+    cout << hex << Pnumber/2 << endl;
+
+    cout<<"------------"<<endl;
+    int ar[5] = {(int) 0xFFFFFFFF, 0x00};
+    int index = 0;
+    index = getMaxBit(ar,5);
+    cout << index<< endl;
+
+
 
     return 0;
 }
