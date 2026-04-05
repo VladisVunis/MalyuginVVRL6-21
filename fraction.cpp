f3768fe31993e203148bd308efc38e6ebd3395b0
Vladimir <dippeepthegood@gmail.com>
2026-04-05 14:47:38 +0300

    rk_1_2_0

--- fraction.cpp
+++ fraction.cpp
@@ -1,31 +1,42 @@
 #include "fraction.h"
+#include <iostream>
 
 int fracReduction(Fraction& frac){
     int a = frac._numerator;
     int b = frac._denominator;
     int ost = 0;
+    a = abs(a);
+    b = abs(b);
     if(b == 0){
         return -1;
     }
     else{
-        if(a > b){
-            while( b != 0){
+        if(abs(a) > abs(b)){
+            while( abs(b) != 0){
                 ost = a%b;
                 a = b;
                 b = ost;
             }
             frac._numerator = frac._numerator/a;
             frac._denominator = frac._denominator/a;
+            if(frac._denominator < 0){
+                frac._numerator = frac._numerator*-1;
+                frac._denominator = frac._denominator* -1;
+            }
             return 0;
         }
         else{
-            while( a != 0){
+            while( abs(a) != 0){
                 ost = b%a;
                 b = a;
                 a = ost;
             }
             frac._numerator = frac._numerator/b;
             frac._denominator = frac._denominator/b;
+            if(frac._denominator < 0){
+                frac._numerator = frac._numerator*-1;
+                frac._denominator = frac._denominator* -1;
+            }
             return 0;
         }
     }
--- main.cpp
+++ main.cpp
@@ -4,17 +4,144 @@
 #include <time.h>
 #include "sorts.h"
 #include "file_operations.h"
-//#include "box.h"
-#include "fraction.h"
+//#include "fraction.h"
+
+#define NAME 1// RK = 1/HW = 2/ SW = 3
+#define COUNT 1 // номер работы
+#define NUMBER 1 // номер задания
+#if NAME == 1 && COUNT == 1
 #include "rk_1.h"
+#endif
+
+#define BOX_VERSE 0
+#if BOX_VERSE == 1
+#include "box.h"
+#elif BOX_VERSE == 2
+#include "rk_1.h"
+#elif BOX_VERSE == 0
+#endif
+struct Fraction {
+    int _numerator; //< числитель
+    int _denominator; //< знаменатель
+    Fraction(){
+        _numerator = 1;
+        _denominator = 1;
+    };
+    Fraction(int numerator, int denominator){
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+};
+
+std::ostream& operator<<(std::ostream& out, const Fraction& FR){
+    out << FR._numerator;
+    out << "/";
+    out << FR._denominator;
+    return out;
+}
+void EVKLID(Fraction& frt){
+    int a = frt._numerator;
+    int b = frt._denominator;
+    int ost = 0;
+    if(b ==0){
+        return;
+    }
+    if(a == 0){
+        frt._denominator = 1;
+        return;
+    }
+    a = abs(a);
+    b = abs(b);
+    if(abs(a) > abs(b)){
+        while( abs(b) != 0){
+            ost = abs(a)%abs(b);
+            a = b;
+            b = ost;
+        }
+        frt._numerator = frt._numerator/a;
+        frt._denominator = frt._denominator/a;
+    }
+    else{
+        while( abs(a) != 0){
+            ost = abs(b)%abs(a);
+            b = a;
+            a = ost;
+        }
+        frt._numerator = frt._numerator/b;
+        frt._denominator = frt._denominator/b;
+    }
+    if(frt._denominator < 0){
+        frt._numerator = frt._numerator*-1;
+        frt._denominator = frt._denominator* -1;
+    }
+}
+
+Fraction sum(const Fraction& fr1, const Fraction& fr2){
+    int a1 = fr1._numerator;
+    int b1 = fr1._denominator;
+    int a2 = fr2._numerator;
+    int b2 = fr2._denominator;
+    Fraction fr0;
+    fr0._numerator = a1 * b2 + a2 * b1;
+    fr0._denominator = b1 * b2;
+    EVKLID(fr0);
+    return fr0;
+}
+
+Fraction sub(const Fraction& fr1, const Fraction& fr2){
+    int a1 = fr1._numerator;
+    int b1 = fr1._denominator;
+    int a2 = fr2._numerator;
+    int b2 = fr2._denominator;
+    Fraction fr0;
+    fr0._numerator = a1 * b2 - a2 * b1;
+    fr0._denominator = b1 * b2;
+    EVKLID(fr0);
+    return fr0;
+}
+
+Fraction div(const Fraction& fr1, const Fraction& fr2){
+    int a1 = fr1._numerator;
+    int b1 = fr1._denominator;
+    int a2 = fr2._numerator;
+    int b2 = fr2._denominator;
+    Fraction fr0;
+    fr0._numerator = a1 * b2;
+    fr0._denominator = b1 * a2;
+    EVKLID(fr0);
+    return fr0;
+
+}
+
+Fraction mul(const Fraction& fr1, const Fraction& fr2){
+    int a1 = fr1._numerator;
+    int b1 = fr1._denominator;
+    int a2 = fr2._numerator;
+    int b2 = fr2._denominator;
+    Fraction fr0;
+    fr0._numerator = a1 * a2;
+    fr0._denominator = b1 * b2;
+    EVKLID(fr0);
+    return fr0;
+
+}
+
+
 
 using namespace std;
 
 int main()
 {
+    Fraction fr1(10, 20);
+    Fraction fr2(-5, 7);
+    std::cout << sum(fr1, fr2) << std::endl;
+    std::cout << sub(fr1, fr2) << std::endl;
+    std::cout << div(fr1,fr2) << std::endl;
+    std::cout << mul(fr1, fr2) << std::endl;
 
 
 
+#if BOX_VERSE == 2
     const struct Box temp(15);
     std::cout << temp;
 
@@ -23,16 +150,17 @@ int main()
 
     const struct Box temp2(5,10,11);
     std::cout << temp2;
+#endif
 
     /*Fraction test(10, 15);
     int res = fracReduction(test);
     if (res == -1) {
         std::cerr << "errror..." << std::endl;
     }
-    std::cout << test._numerator << "/" << test._denominator << std::endl;
+    std::cout << test._numerator << "/" << test._denominator << std::endl;*/
 
-
-    /*Box box;
+#if BOX_VERSE == 1
+    Box box;
     Box* temp = new Box();
 
     temp->_len = 1;
@@ -50,7 +178,7 @@ int main()
     cout << std::hex << temp1->_color <<endl;
     delete temp1;
     temp1 = nullptr;
-
+#endif
     /*srand(time(nullptr));
     fillFile("set", 10, 20, '\t', 7, 12);*/
 
